4857aaa37d2025b3792289a8b8611bc61a0f0793
Amit Ofeck <Amitofeck1@gmail.com>
2025-02-24 20:46:25 +0200

    navigate + initial login and dashboard fragmant

---
+++ app/src/main/java/com/example/ibookproject/ui/auth/LoginFragment.kt
@@ -0,0 +1,32 @@
+package com.example.ibookproject.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.ibookproject.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.root.setOnClickListener {
+            findNavController().navigate(com.example.ibookproject.R.id.action_loginFragment_to_dashboardFragment)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
